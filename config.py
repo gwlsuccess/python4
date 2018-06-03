a87b43241865728f2525a29db1a7cd23214595de
xwp <xwp_fullstack@163.com>
2018-06-03 08:35:41 +0800

    导入各类模块，实例化各对象，配置文件

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 class Config:
     DEBUG=True
+    # 设置数据库连接
+    SQLALCHEMY_DATABASE_URL='mysql://root:mysql@localhost/test3'
+    # 配置数据库的动态追踪修改
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_script import Manager
 from config import Config
-
+from flask_migrate import Migrate,MigrateCommand
 app = Flask(__name__)
+db =Migrate(app)
 manager =Manager(app)
+manager.add_command('db',MigrateCommand)
 app.config.from_object(Config)
 @app.route('/')
 def index():
