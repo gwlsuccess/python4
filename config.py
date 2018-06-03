04d96c5577e79ef5e153860ddea0eca34447d8b8
xwp <xwp_fullstack@163.com>
2018-06-03 08:48:01 +0800

    导入redis，CSRFProtecct,Session

--- config.py
+++ config.py
@@ -1,6 +1,24 @@
+from redis import StrictRedis
 class Config:
+
     DEBUG=True
+    # 设置密钥
+    SECRET_KEY = 'heyKyqaUgg8jAJJvjwxy3bUCkBFBX5ao3kK0HLptbW8='
+
     # 设置数据库连接
     SQLALCHEMY_DATABASE_URL='mysql://root:mysql@localhost/test3'
     # 配置数据库的动态追踪修改
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 配置redis的主机和端口
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
+    # 使用redis来保存session信息
+    SESSION_TYPE = 'redis'
+    # 对象session信息进行签名
+    SESSION_USE_SIGNER = True
+    # 存储session的redis实例
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 指定session的过期时间1天
+    PERMANENT_SESSION_LIFETIME = 86400
--- manage.py
+++ manage.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask,session
 from flask_script import Manager
 from config import Config
 from flask_migrate import Migrate,MigrateCommand
+from flask_session import Session
+from flask_wtf import CSRFProtect
 app = Flask(__name__)
+Session(app)
 db =Migrate(app)
+CSRFProtect(app)
 manager =Manager(app)
 manager.add_command('db',MigrateCommand)
 app.config.from_object(Config)
 @app.route('/')
 def index():
+    session['name']='2022'
     return 'index'
 
 if __name__ == '__main__':
